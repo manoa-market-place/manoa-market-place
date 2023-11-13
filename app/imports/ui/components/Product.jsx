b6a81e2bb85cfab2ace4a4c6cf53a251126a8263
Yujie He <yujie6@hawaii.edu>
2023-11-12 21:22:27 -1000

    add product

---
+++ app/imports/ui/components/Product.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card } from 'react-bootstrap';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+const Product = ({ product }) => (
+  <Card className="h-100">
+    <Card.Header>
+      <Card.Title>{product.name}</Card.Title>
+      <Card.Subtitle>{product.price}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{product.description}</Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+// Require a document to be passed to this component.
+Product.propTypes = {
+  product: PropTypes.shape({
+    name: PropTypes.string,
+    price: PropTypes.number,
+    image: PropTypes.string,
+    category: PropTypes.string,
+    description: PropTypes.string,
+    // _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default Product;
---
+++ app/imports/ui/pages/AddProduct.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { AutoForm, ErrorsField, LongTextField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { Products } from '../../api/product/Products';
+
+// Create a schema to specify the structure of the data to appear in the form.
+const formSchema = new SimpleSchema({
+  name: String,
+  price: Number,
+  image: String,
+  category: String,
+  description: String,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+/* Renders the AddStuff page for adding a document. */
+const AddProduct = () => {
+
+  // On submit, insert the data.
+  const submit = (data, formRef) => {
+    const { name, price, image, category, description } = data;
+    const owner = Meteor.user().username;
+    Products.collection.insert(
+      { name, price, image, category, description, owner },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Item added successfully', 'success');
+          formRef.reset();
+        }
+      },
+    );
+  };
+
+  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
+  let fRef = null;
+  return (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={5}>
+          <Col className="text-center"><h2>Add Stuff</h2></Col>
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
+            <Card>
+              <Card.Body>
+                <TextField name="name" />
+                <TextField name="image" />
+                <TextField name="price" />
+                <TextField name="category" />
+                <LongTextField name="description" />
+                <SubmitField value="Submit" />
+                <ErrorsField />
+              </Card.Body>
+            </Card>
+          </AutoForm>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default AddProduct;
