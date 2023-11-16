8bd0cc84826ee770fc376f44197885b780fd5d55
Andrew Lin <143560794+andrewlin808@users.noreply.github.com>
2023-11-15 17:50:49 -1000

    updated landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { HouseAddFill } from 'react-bootstrap-icons';
+import { CartFill, HouseAddFill, PeopleFill } from 'react-bootstrap-icons';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container id="landing-page" fluid className="py-3">
-    <Row className="align-middle text-center">
+  <Container id="landing-page" fluid className="py-2">
+    <Row className="align-middle text-center py-5">
       <Col>
         <h1>Manoa Marketplace</h1>
         <p>Your online store for UHM student-related goods and services</p>
       </Col>
     </Row>
-    <Row className="align-middle text-center">
+    <Row className="text-center py-2">
       <Col xs={4}>
-        Some Good Icon
-        <h3>Goods</h3>
-        <p>Look for something to buy/sell.</p>
+        <CartFill size={50} />
       </Col>
       <Col xs={4}>
-        Some Service Icon
-        <h3>Services</h3>
-        <p>Advertise a skill for commission.</p>
+        <PeopleFill size={50} />
       </Col>
       <Col xs={4}>
         <HouseAddFill size={50} />
-        <h3>Accommodations</h3>
+      </Col>
+    </Row>
+    <Row className="text-center">
+      <Col xs={4}>
+        <h3 className="pt-5">Goods</h3>
+        <p>Look for something to buy/sell.</p>
+      </Col>
+      <Col xs={4}>
+        <h3 className="pt-5">Services</h3>
+        <p>Advertise a skill for commission.</p>
+      </Col>
+      <Col xs={4}>
+        <h3 className="pt-5">Accommodations</h3>
         <p>Look for a housing accommodation.</p>
       </Col>
     </Row>
