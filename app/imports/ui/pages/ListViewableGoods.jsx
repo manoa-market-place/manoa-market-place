649de7a27e9809403696cf766da8dbc85cae0bf9
Derealization-kk <bingying.li02@gmail.com>
2023-11-28 02:40:05 -1000

    filter update

--- app/imports/ui/pages/ListViewableGoods.jsx
+++ app/imports/ui/pages/ListViewableGoods.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Col, Container, Row } from 'react-bootstrap';
@@ -10,6 +10,7 @@ import { ProductsInCart } from '../../api/product/ProductsInCart';
 /* Renders a table containing all of the product documents. Use <productItemAdmin> to render each row. */
 const ListViewableGoods = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const [conditionFilter, setConditionFilter] = useState('');
   const { products, cart, ready } = useTracker(() => {
     // Get access to product documents.
     const ProductsSubscription = Meteor.subscribe(Products.allPublicationName);
@@ -26,7 +27,11 @@ const ListViewableGoods = () => {
     };
   }, []);
 
-  const goods = products.filter((product => (product.owner !== Meteor.user().username)));
+  const goods = products.filter((product) => product.owner !== Meteor.user().username);
+  const filteredGoods = goods.filter((good) => !conditionFilter || good.condition === conditionFilter);
+  const handleConditionFilterChange = (event) => {
+    setConditionFilter(event.target.value);
+  };
 
   return (ready ? (
     <Container className="py-3">
@@ -35,10 +40,18 @@ const ListViewableGoods = () => {
           <Col className="text-center">
             <h2>Goods for Sale</h2>
           </Col>
+          <Col className="text-center">
+            <input
+              type="text"
+              placeholder="Filter by condition"
+              value={conditionFilter}
+              onChange={handleConditionFilterChange}
+            />
+          </Col>
         </Col>
       </Row>
       <Row xs={1} md={2} lg={3}>
-        {goods.map((product) => (<Col key={product._id} className="gy-4"><ViewableGood good={product} cartCollection={cart} /></Col>))}
+        {filteredGoods.map((product) => (<Col key={product._id} className="gy-4"><ViewableGood good={product} cartCollection={cart} /></Col>))}
       </Row>
     </Container>
   ) : <LoadingSpinner />);
