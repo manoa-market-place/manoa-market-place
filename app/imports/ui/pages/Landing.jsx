899011fcd1d8801bc8db2399fc3c2dfcf1c73305
Andrew Lin <143560794+andrewlin808@users.noreply.github.com>
2023-11-28 11:16:47 -1000

    Update Landing.jsx
    Still need to fix Landing Page's info

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -29,6 +29,47 @@ const Landing = () => (
         <p>Advertise a skill for commission.</p>
       </Col>
     </Row>
+    <Row className="overview text-center">
+      <h2>Overview</h2>
+      <p>Idle goods trading platform, let you anytime, anywhere sell fast, buy well, reliable and easy to solve idle goods trouble!</p>
+    </Row>
+    <Row className="offer text-center">
+      <h2>What We Offer</h2>
+      <Row>
+        <Col>
+          <p>Transaction Assistance</p>
+        </Col>
+        <Col>
+          <p>Account Management</p>
+        </Col>
+        <Col>
+          <p>Technical Support</p>
+        </Col>
+        <Col>
+          <p>Dispute Resolution</p>
+        </Col>
+        <Col>
+          <p>Educational Resources</p>
+        </Col>
+      </Row>
+    </Row>
+
+    <Row className="intro text-center">
+      <h2>Cash</h2>
+      <p>take pictures and sell, idle items around you become valuable in 10 seconds</p>
+      <h2>Save Money</h2>
+      <p>new idle value second-hand waiting for you to search</p>
+      <h2>Environmental protection</h2>
+      <p>eliminate waste, idle can also glow new value</p>
+    </Row>
+
+    <Row className="vendor-information text-center">
+      <h2>Become a Vendor</h2>
+      <p>Interested in selling your goods? Join our vibrant community of vendors! Contact us for booth availability and pricing.</p>
+      <p>feel free to contact Us if you have any questions</p>
+      <p>andrewl2@hawaii.edu</p>
+      <p>Phone: (206) 403-6299</p>
+    </Row>
   </Container>
 );
 
